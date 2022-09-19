78144c9fc2bb12ad535f94ee676b04e97bd8aa2f
M&B Code <105244471+Bountukp20@users.noreply.github.com>
2022-09-19 17:40:30 -0500

    Add files via upload

---
+++ subscriberform.php
@@ -0,0 +1,16 @@
+<?php
+
+
+if($_POST["firstname, lastname, email, country, subject"]) {
+
+
+mail("marvcode.co@email.address", "Contact",
+
+
+$_POST["insert your message here"]. "From: an@email.address");
+
+
+}
+
+
+?>
